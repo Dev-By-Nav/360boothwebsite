e67d4cfb693429aa67d5b03002f94aa200fffad1
Navneet <itsnav3@gmail.com>
2023-08-07 14:56:09 +0100

    retrying

--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -1,16 +1,16 @@
 // src/Videos.js
 import React from 'react';
-import Video1 from '../Videos/vid1.MP4';
+import Video2 from '../Videos/vid2.MP4';
 
 function Videos() {
-  const videoList = [Video1];
+  const videoList = [Video2, Video2];
 
   return (
     <div className="max-w-[90vw] mx-auto columns-1  xs:columns-2 md:columns-3 lg:columns-4 xl:columns-5 space-y-4 my-6">
       {videoList.map((video, index) => (
         <div key={index}>
           <video className="w-full" controls muted>
-            <source src={video} type="video/MP4" />
+            <source src={video} type="video/mp4" />
           </video>
         </div>
       ))}
