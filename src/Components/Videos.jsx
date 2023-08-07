40c3e934ba0112000852a5aa8bc7e37d83aacece
Navneet <itsnav3@gmail.com>
2023-08-07 14:35:55 +0100

    old vids

--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -1,24 +1,11 @@
 // src/Videos.js
 import React from 'react';
-import Video1 from '../Videos/vid1.MP4';
 import Video2 from '../Videos/vid2.MP4';
-import Video3 from '../Videos/vid3.MP4';
-import Video4 from '../Videos/vid4.MP4';
-import Video5 from '../Videos/vid5.MP4';
-import Video6 from '../Videos/vid6.MP4';
-import Video7 from '../Videos/vid7.MP4';
-import Video8 from '../Videos/vid9.MP4';
-import Video9 from '../Videos/vid10.MP4';
-import Video10 from '../Videos/vid11.MP4';
-import Video11 from '../Videos/vid12.MP4';
-import Video12 from '../Videos/vid14.MP4';
-import Video13 from '../Videos/vid15.MP4';
-import Video14 from '../Videos/vid16.MP4';
-
+import Video3 from '../Videos/vid5.MP4';
 
 function Videos() {
-  const videoList = [Video1, Video2, Video3, Video4, Video5, Video6, Video7, Video8, Video9, Video10, Video11, Video12, Video13, Video14 ];
- 
+  const videoList = [Video2, Video3, Video3, Video3, Video2];
+
   return (
     <div className="max-w-[90vw] mx-auto columns-1  xs:columns-2 md:columns-3 lg:columns-4 xl:columns-5 space-y-4 my-6">
       {videoList.map((video, index) => (
