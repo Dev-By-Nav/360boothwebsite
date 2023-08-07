9e21870f894088225b0437bdd2a36ecb4b2bb0ec
Navneet <itsnav3@gmail.com>
2023-08-07 14:31:45 +0100

    VIDEO retry

--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -1,19 +1,19 @@
 // src/Videos.js
 import React from 'react';
-import Video1 from '../Videos/vid1.mp4';
-import Video2 from '../Videos/vid2.mp4';
-import Video3 from '../Videos/vid3.mp4';
-import Video4 from '../Videos/vid4.mp4';
-import Video5 from '../Videos/vid5.mp4';
-import Video6 from '../Videos/vid6.mp4';
-import Video7 from '../Videos/vid7.mp4';
-import Video8 from '../Videos/vid9.mp4';
-import Video9 from '../Videos/vid10.mp4';
-import Video10 from '../Videos/vid11.mp4';
-import Video11 from '../Videos/vid12.mp4';
-import Video12 from '../Videos/vid14.mp4';
-import Video13 from '../Videos/vid15.mp4';
-import Video14 from '../Videos/vid16.mp4';
+import Video1 from '../Videos/vid1.MP4';
+import Video2 from '../Videos/vid2.MP4';
+import Video3 from '../Videos/vid3.MP4';
+import Video4 from '../Videos/vid4.MP4';
+import Video5 from '../Videos/vid5.MP4';
+import Video6 from '../Videos/vid6.MP4';
+import Video7 from '../Videos/vid7.MP4';
+import Video8 from '../Videos/vid9.MP4';
+import Video9 from '../Videos/vid10.MP4';
+import Video10 from '../Videos/vid11.MP4';
+import Video11 from '../Videos/vid12.MP4';
+import Video12 from '../Videos/vid14.MP4';
+import Video13 from '../Videos/vid15.MP4';
+import Video14 from '../Videos/vid16.MP4';
 
 
 function Videos() {
