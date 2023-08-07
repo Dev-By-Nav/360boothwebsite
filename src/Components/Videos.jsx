6691d186de1ecd67847bc03a23a990a68e94b330
Navneet <itsnav3@gmail.com>
2023-08-07 14:44:14 +0100

    retry video

--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -1,10 +1,9 @@
 // src/Videos.js
 import React from 'react';
-import Video2 from '../Videos/vid2.MP4';
-import Video3 from '../Videos/vid5.MP4';
+import Video1 from '../Videos/Vid1.MP4';
 
 function Videos() {
-  const videoList = [Video2, Video3, Video3, Video3, Video2];
+  const videoList = [Video1];
 
   return (
     <div className="max-w-[90vw] mx-auto columns-1  xs:columns-2 md:columns-3 lg:columns-4 xl:columns-5 space-y-4 my-6">
